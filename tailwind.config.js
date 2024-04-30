442e0fda492a5c77bc2ecb91e45795b171ffbf93
paolotiu <paolotiu17@gmail.com>
2024-04-30 19:18:51 +0800

    Tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      black: '#0d0b10',
+      'dark-green': '#0c4346',
+      'light-Green': '#85e0a3',
+      'light-red': '#e46351',
+      'dark-red': '#4d0505',
+      white: '#ffffff',
+      't-dark-green': '#111f15',
+      pink: '#ffd8d3',
+    },
   },
   plugins: [],
 };
